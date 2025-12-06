a5fa648acb23cccb02257e313f418db4cc8840bc
amitpc83 <achowdhary@outlook.com>
2025-12-06 16:14:45 +0100

    Refactored Home Page

---
+++ PizzaPlace/Components/Pages/CustomerEntry.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shared;
+
+namespace PizzaPlace.Components.Pages
+{
+    public partial class CustomerEntry
+    {
+        [Parameter]
+        public string Title { get; set; } = default!;
+        [Parameter]
+        public string ButtonTitle { get; set; } = default!;
+        [Parameter]
+        public string ButtonClass { get; set; } = default!;
+        [Parameter]
+        public Customer Customer { get; set; } = default!;
+        [Parameter]
+        public EventCallback ValidSubmit { get; set; } = default!;
+        [Parameter]
+        public EventCallback<Customer> CustomerChanged { get;set; }
+
+        private InputWatcher inputWatcher = default!;
+        bool isInvalid = true;
+
+        private void FieldChanged(string fieldName)
+        {
+            CustomerChanged.InvokeAsync(Customer);
+            isInvalid = !inputWatcher.Validate();
+        }
+    }
+}
